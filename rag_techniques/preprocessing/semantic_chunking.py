84f7b134d3323929251964f95510b7765217f6b0
chrysniongolo <164155876+chrysniongolo@users.noreply.github.com>
2024-10-10 00:50:39 +0200

    feat: Add semantic chunking functionality with new dependencies

--- rag_techniques/preprocessing/semantic_chunking.py
+++ rag_techniques/preprocessing/semantic_chunking.py
@@ -1,4 +1,7 @@
 from rag_techniques.base import BasePreprocessor
+from langchain_experimental.text_splitter import SemanticChunker, BreakpointThresholdType
+from langchain_openai import OpenAIEmbeddings
+
 
 class SemanticChunking(BasePreprocessor):
     def process(self, data):
