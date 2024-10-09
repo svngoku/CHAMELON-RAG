c6466ed6e68367f96c57057ffde26d2ccc391567
chrysniongolo <164155876+chrysniongolo@users.noreply.github.com>
2024-10-09 22:24:03 +0200

    feat: Add RAGPipeline class for retrieval-augmented generation process

---
+++ rag_techniques/pipeline/rag_pipeline.py
@@ -0,0 +1,40 @@
+# rag_techniques/pipeline.py
+class RAGPipeline:
+    def __init__(self):
+        self.preprocessors = []
+        self.retriever = None
+        self.postprocessors = []
+        self.generator = None
+
+    def add_preprocessor(self, preprocessor):
+        self.preprocessors.append(preprocessor)
+        return self
+
+    def set_retriever(self, retriever):
+        self.retriever = retriever
+        return self
+
+    def add_postprocessor(self, postprocessor):
+        self.postprocessors.append(postprocessor)
+        return self
+
+    def set_generator(self, generator):
+        self.generator = generator
+        return self
+
+    def run(self, query, data):
+        # Preprocess data
+        for preprocessor in self.preprocessors:
+            data = preprocessor.process(data)
+
+        # Retrieve relevant information
+        retrieved_data = self.retriever.retrieve(query)
+
+        # Postprocess retrieved data
+        for postprocessor in self.postprocessors:
+            retrieved_data = postprocessor.process(retrieved_data)
+
+        # Generate response
+        response = self.generator.generate(retrieved_data, query)
+
+        return response
